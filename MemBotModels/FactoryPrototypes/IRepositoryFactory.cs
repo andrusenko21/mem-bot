8e386d1862289504d1f46ebda03985992e5993dc
andrusenko21 <o.andrusenko97@gmail.com>
2019-12-21 16:14:57 +0200

    Refactoring

--- MemBotModels/FactoryPrototypes/IRepositoryFactory.cs
+++ MemBotModels/FactoryPrototypes/IRepositoryFactory.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MemBotModels.FactoryPrototypes
+﻿namespace MemBotModels.FactoryPrototypes
 {
     public interface IRepositoryFactory
     {
